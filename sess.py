5694636e167d823a83468b9b08793b8410c3f13f
Hongzhi Song <dldz2002@gmail.com>
2017-02-03 11:12:45 +0800

    An example on session

---
+++ sess.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import tensorflow as tf
+
+matrix1 = tf.constant([[3, 3]])
+matrix2 = tf.constant([[2],
+                       [2]])
+product = tf.matmul(matrix1, matrix2)
+
+# sess = tf.Session()
+# result = sess.run(product)
+# print(result)
+# sess.close()
+
+# Or use `with` statement
+with tf.Session() as sess:
+    result2 = sess.run(product)
+    print(result2)
