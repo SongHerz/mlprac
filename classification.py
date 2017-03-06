ae32120a145593b7615f80698a905c3533261f95
Hongzhi Song <dldz2002@gmail.com>
2017-03-07 00:16:39 +0800

    Some comments on observation

--- classification.py
+++ classification.py
@@ -13,7 +13,9 @@ mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 
 def add_layer(inputs, in_size, out_size, activation_function=None):
-    Weights = tf.Variable(tf.random_normal([in_size, out_size]))
+    # Weights = tf.Variable(tf.random_normal([in_size, out_size]))
+    Weights = tf.Variable(tf.zeros([in_size, out_size]))
+    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
     biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
     Wx_plus_b = tf.matmul(inputs, Weights) + biases
     if activation_function is None:
@@ -40,6 +42,7 @@ ys = tf.placeholder(tf.float32, (None, OUT_SIZE))
 prediction = add_layer(xs, IN_SIZE, OUT_SIZE, activation_function=tf.nn.softmax)
 
 # loss
+# Call this "naive cross_entropy"
 # cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction),
 #                                                reduction_indices=[1]))
 # FIXME: It looks convergence speed is much slower with softmax_cross_entropy_with_logits
@@ -53,10 +56,21 @@ train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
-    for i in range(8000):
+    for i in range(1000):
         batch_xs, batch_ys = mnist.train.next_batch(100)
         sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys})
         if i % 50 == 0:
             print(compute_accuracy(
                 sess, prediction,
                 mnist.test.images, mnist.test.labels))
+
+
+# FIXME: I HAVE NO IDEA WHY ???????
+# It is very interesting that:
+# 1. with weights initialized by random_normal:
+# 1.1 naive cross_entropy convergent fast.
+# 1.2 softmax_cross_entropy_with_logits convergent very slow.
+#
+# 2. with weights initialized by zeros:
+# 1.1 naive cross_entropy convergent a little faster than softmax_cross_entropy_with_logits
+# 1.2 softmax_cross_entropy_with_logits fast
